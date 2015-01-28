8c00490770be0d537e62d5f7d629c68de2bda806
Ben Roaman <benroaman@gmail.com>
2015-01-28 00:40:38 -0500

    added multi-menu functionality to dropmenu, added click body to hide drop and stop propagation in each main button

--- dropmenu/jsdropdown.js
+++ dropmenu/jsdropdown.js
@@ -1,17 +1,80 @@
-function toggleClass (element, className) {
-  var currentClasses = element.className;
+// function toggleClass (element, className) {
+//   var currentClasses = element.className;
+//
+//   if (currentClasses.indexOf(className) >=0) {
+//     element.className = currentClasses.replace(className, '').trim();
+//   } else {
+//     element.className += ' ' + className;
+//   }
+// }
+//
+// var mainButtonA = document.querySelector('.dropmenu-activate');
+//
+// mainButtonA.addEventListener('click', function() {
+//   toggleClass(mainButtonA, 'visible');
+// })
 
-  if (currentClasses.indexOf(className) >=0) {
-    element.className = currentClasses.replace(className, '').trim();
+//above is the cleaner version for one button
+
+var mainButtonA = document.querySelector('#menu-a');
+var mainButtonB = document.querySelector('#menu-b'); // instancing all 3 menus in the script
+var mainButtonC = document.querySelector('#menu-c');
+
+var body = document.querySelector('body');
+
+body.addEventListener('click', function() {
+  var aClasses = mainButtonA.className;
+  var bClasses = mainButtonB.className;
+  var cClasses = mainButtonC.className;
+
+  mainButtonA.className = aClasses.replace('visible', '').trim();
+  mainButtonB.className = bClasses.replace('visible', '').trim();
+  mainButtonC.className = cClasses.replace('visible', '').trim();
+})
+
+mainButtonA.addEventListener('click', function(e) { // making listener for menu A
+  e.stopPropagation();
+  var currentClasses = mainButtonA.className;
+  var bClasses = mainButtonB.className;
+  var cClasses = mainButtonC.className;
+
+  if (currentClasses.indexOf('visible') >=0) {
+    mainButtonA.className = currentClasses.replace('visible', '').trim();
   } else {
-    element.className += ' ' + className;
+    mainButtonA.className += ' ' + 'visible';
+    mainButtonB.className = bClasses.replace('visible', '').trim(); // resetting other menus
+    mainButtonC.className = cClasses.replace('visible', '').trim();
   }
-}
+})
 
-var mainButtonA = document.querySelector('.dropmenu-activate');
+mainButtonB.addEventListener('click', function(e) {
+  e.stopPropagation();
+  var currentClasses = mainButtonB.className;
+  var aClasses = mainButtonA.className;
+  var cClasses = mainButtonC.className;
 
-mainButtonA.addEventListener('click', function() {
-  toggleClass(mainButtonA, 'visible');
+  if (currentClasses.indexOf('visible') >=0) {
+    mainButtonB.className = currentClasses.replace('visible', '').trim();
+  } else {
+    mainButtonB.className += ' ' + 'visible';
+    mainButtonA.className = aClasses.replace('visible', '').trim();
+    mainButtonC.className = cClasses.replace('visible', '').trim();
+  }
+})
+
+mainButtonC.addEventListener('click', function(e) {
+  e.stopPropagation();
+  var currentClasses = mainButtonC.className;
+  var aClasses = mainButtonA.className;
+  var bClasses = mainButtonB.className;
+
+  if (currentClasses.indexOf('visible') >=0) {
+    mainButtonC.className = currentClasses.replace('visible', '').trim();
+  } else {
+    mainButtonC.className += ' ' + 'visible';
+    mainButtonA.className = aClasses.replace('visible', '').trim();
+    mainButtonB.className = bClasses.replace('visible', '').trim();
+  }
 })
 
 //The code below was meant to add functionality to all three buttons,
